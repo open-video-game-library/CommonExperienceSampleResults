0ab462a352587f19b4f6d603368eab6960acd0e2
ttttt.fms5@gmail.com <ttttt.fms5@gmail.com>
2023-01-20 15:56:38 +0900

    change base url

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/CommonExperienceSample_results/",
   plugins: [
     vue({ 
       template: { transformAssetUrls }
